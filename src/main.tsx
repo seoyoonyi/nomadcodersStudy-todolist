0fcc2d87f4ed16a2471b441ea324cbdde4a10adf
seoyoonyi <yiseoyoon@gmail.com>
2023-03-01 12:02:38 +0900

    feat:: todolist 기능 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+//
+import App from './App';
+import { HelmetProvider } from 'react-helmet-async';
+import { Home } from './page/Home';
+import { NotFound } from './page/NotFound';
+import { RecoilRoot } from 'recoil';
+
+const router = createBrowserRouter(
+	[
+		{
+			path: '/',
+			element: <App />,
+			errorElement: <NotFound />,
+			children: [{ index: true, element: <Home /> }],
+		},
+	],
+	{
+		basename: import.meta.env.BASE_URL,
+	},
+);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<HelmetProvider>
+			<RecoilRoot>
+				<App />
+				<RouterProvider router={router} />
+			</RecoilRoot>
+		</HelmetProvider>
+	</React.StrictMode>,
+);
---
+++ src/page/Home.tsx
@@ -0,0 +1,27 @@
+import { useRecoilState, useRecoilValue } from 'recoil';
+import CreateToDo from '../components/CreateToDo';
+import ToDo from '../components/ToDo';
+import { categoryState, toDoSelector } from '../context/atmos';
+
+export const Home = () => {
+	const toDos = useRecoilValue(toDoSelector);
+	const [category, setCategory] = useRecoilState(categoryState);
+	const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+		setCategory(event.currentTarget.value);
+	};
+	return (
+		<div>
+			<h1>To Dos</h1>
+			<hr />
+			<select value={category} onInput={onInput}>
+				<option value="TO_DO">To Do</option>
+				<option value="DOING">Doing</option>
+				<option value="DONE">Done</option>
+			</select>
+			<CreateToDo />
+			{toDos?.map((toDo) => (
+				<ToDo key={toDo.id} {...toDo} />
+			))}
+		</div>
+	);
+};
