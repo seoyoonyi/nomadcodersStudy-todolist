02db1d55d22bcebe0055da028c9307201aa33d45
seoyoonyi <yiseoyoon@gmail.com>
2023-03-01 12:46:27 +0900

    feat:: 카테고리 커스텀 기능 추가

--- src/context/atmos.tsx
+++ src/context/atmos.tsx
@@ -6,14 +6,38 @@ export interface IToDo {
 	category: 'TO_DO' | 'DOING' | 'DONE';
 }
 
+export let defaultCategories: string[] = ['TO_DO', 'DOING', 'DONE'];
+
+const localStorageEffect =
+	(key: string) =>
+	({ setSelf, onSet }: any) => {
+		const savedValue = localStorage.getItem(key);
+		// setSelf -> Callbacks to set or reset the value of the atom.
+		if (savedValue != null) {
+			setSelf(JSON.parse(savedValue));
+		}
+
+		// onSet -> Subscribe to changes in the atom value.
+		onSet((newValue: any, _: any, isReset: boolean) => {
+			isReset ? localStorage.removeItem(key) : localStorage.setItem(key, JSON.stringify(newValue));
+		});
+	};
+
 export const categoryState = atom({
 	key: 'category',
-	default: 'TO_DO',
+	default: defaultCategories[0],
+});
+
+export const categoriesState = atom<string[]>({
+	key: 'categoriesState',
+	default: JSON.parse(localStorage.getItem('categories') ?? JSON.stringify(defaultCategories)),
+	effects: [localStorageEffect('categoriesState')],
 });
 
 export const toDoState = atom<IToDo[]>({
 	key: 'toDo',
 	default: [],
+	effects: [localStorageEffect('toDo')],
 });
 
 export const toDoSelector = selector({
--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,23 +1,51 @@
 import { useRecoilState, useRecoilValue } from 'recoil';
 import CreateToDo from '../components/CreateToDo';
 import ToDo from '../components/ToDo';
-import { categoryState, toDoSelector } from '../context/atmos';
+import { categoriesState, categoryState, toDoSelector } from '../context/atmos';
 
 export const Home = () => {
 	const toDos = useRecoilValue(toDoSelector);
 	const [category, setCategory] = useRecoilState(categoryState);
-	const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
-		setCategory(event.currentTarget.value);
+	const [categories, setCategories] = useRecoilState(categoriesState);
+
+	const onClick = (category: string) => {
+		setCategory(category);
+	};
+
+	const addCategory = () => {
+		const newCategory = prompt('What is the name of the new category?', '');
+
+		if (newCategory) {
+			if (categories.includes(newCategory)) {
+				alert("Can't add a category with the same name because it already exists.");
+				return;
+			}
+
+			setCategories([...categories, newCategory]);
+			setCategory(newCategory);
+		}
 	};
 	return (
 		<div>
 			<h1>To Dos</h1>
 			<hr />
-			<select value={category} onInput={onInput}>
-				<option value="TO_DO">To Do</option>
-				<option value="DOING">Doing</option>
-				<option value="DONE">Done</option>
-			</select>
+
+			<div>
+				{categories.map((availableCategory) => (
+					<div key={availableCategory}>
+						<button
+							onClick={() => onClick(availableCategory)}
+							disabled={availableCategory === category}
+						>
+							{availableCategory}
+						</button>
+					</div>
+				))}
+				<div>
+					<button onClick={addCategory}>+</button>
+				</div>
+			</div>
+
 			<CreateToDo />
 			{toDos?.map((toDo) => (
 				<ToDo key={toDo.id} {...toDo} />
